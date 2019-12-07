c615810e6581082672fa745ae546a833c4bd33b4
krccrk <ccrkhere@gmail.com>
2019-12-06 21:51:42 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var request = require('request');
+var tableify = require('tableify');
+var json2html = require('node-json2html');
+const app = require('express')();
+app.get("/GetData",(req,resp,next)=>
+{
+    resp.send("Hello");
+})
+app.listen(4000);
+
