9d74be076c71eff91bb8f8eceb613c185e691b81
krccrk <ccrkhere@gmail.com>
2019-12-07 19:11:44 -0700

    Commited with space
    Proper styling

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var port = process.env.PORT || 3000,
     fs = require('fs'),
     html = fs.readFileSync('index.html');
 
+
 var log = function(entry) {
     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
 };
