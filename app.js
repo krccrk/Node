f592228faa5da3dd483dfb746c86dc4f5db9a650
krccrk <ccrkhere@gmail.com>
2019-12-15 11:18:48 -0700

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var server = http.createServer(function (req, res) {
 });
 
 // Leeeisten on port 3000, IP defaults to 127.0.0.1
-server.listen(port,"0.0.0.0");
+server.listen(port,"ec2-3-84-165-50.compute-1.amazonaws.com");
 
 // Put a friendly message on the terminal
 console.log('Server running at http://127.0.0.1:' + port + '/');
